4a89a8abe07b13d23c6642763dca95fa808e987b
Ahmed Amine Bennani <121638453+hmeidas@users.noreply.github.com>
2023-04-15 23:47:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from google.oauth2 import service_account
 from gsheetsdb import connect
 import pandas as pd
 
-# Create a connection object.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=[
@@ -21,33 +20,29 @@ def run_query(query):
 
 def load_data(sheet_url):
     query = f'SELECT * FROM "{sheet_url}"'
-    rows = conn.execute(query)
-    data = pd.DataFrame(rows, columns=["Task", "Status", "Due Date"])
+    rows = run_query(query)
+    data = pd.DataFrame(rows, columns=["Task", "Status"])
     return data
 
-
-# Save data
 def save_data(sheet_url, data):
     conn.execute(f'DELETE FROM "{sheet_url}" WHERE "Task" IS NOT NULL')
     for _, row in data.iterrows():
-        conn.execute(f'INSERT INTO "{sheet_url}" ("Task", "Status", "Due Date") VALUES (%s, %s, %s)', row)
+        conn.execute(f'INSERT INTO "{sheet_url}" ("Task", "Status") VALUES (%s, %s)', row)
 
-# Update task status
 def update_task_status(sheet_url, task, status):
     data = load_data(sheet_url)
     data.loc[data["Task"] == task, "Status"] = status
     save_data(sheet_url, data)
 
-# Streamlit app
 st.title("To-Do List")
 
-sheet_url =  st.secrets["private_gsheets_url"]
+sheet_url = st.secrets["private_gsheets_url"]
 
 if sheet_url:
     data = load_data(sheet_url)
 
     for _, row in data.iterrows():
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
         if row["Status"] == "Pending":
             with col1:
                 check = st.checkbox("Mark as Completed", key=row["Task"])
@@ -55,15 +50,11 @@ if sheet_url:
                     update_task_status(sheet_url, row["Task"], "Completed")
             with col2:
                 st.text(row["Task"])
-            with col3:
-                st.text(row["Due Date"])
         else:
             with col1:
                 st.text("Completed")
             with col2:
                 st.text(row["Task"])
-            with col3:
-                st.text(row["Due Date"])
-                
+
     if st.button("Refresh"):
         data = load_data(sheet_url)
